9a0e27dedfd12579bd893eb631a5b2dbf0975148
Yuxiang Gao <yuxiang.gao@jhu.edu>
2020-06-17 17:58:56 -0400

    Centralized social force

--- crowd_sim/envs/policy/socialforce.py
+++ crowd_sim/envs/policy/socialforce.py
@@ -56,3 +56,34 @@ class SocialForce(Policy):
         self.last_state = state
 
         return action
+
+
+class CentralizedSocialForce(SocialForce):
+    """
+    Centralized socialforce, a bit different from decentralized socialforce, where the goal position of other agents is
+    set to be (0, 0)
+    """
+
+    def __init__(self):
+        super().__init__()
+
+    def predict(self, state):
+        sf_state = []
+        for agent_state in state:
+            sf_state.append(
+                (
+                    agent_state.px,
+                    agent_state.py,
+                    agent_state.vx,
+                    agent_state.vy,
+                    agent_state.gx,
+                    agent_state.gy,
+                )
+            )
+        groups = None
+        sim = Simulator(np.array(sf_state), groups=groups)
+        sim.step()
+        actions = [ActionXY(sim.state[i, 2], sim.state[i, 3]) for i in range(len(state))]
+        del sim
+
+        return actions
