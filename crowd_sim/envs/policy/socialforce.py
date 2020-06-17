b9d1df29d29f40d6b9d52d0ab9a086085fdac43f
Yuxiang Gao <yuxiang.gao@jhu.edu>
2020-06-17 17:42:17 -0400

    Update social force policy

--- crowd_sim/envs/policy/socialforce.py
+++ crowd_sim/envs/policy/socialforce.py
@@ -1,5 +1,5 @@
 import numpy as np
-import socialforce
+from pysocialforce import Simulator
 from crowd_sim.envs.policy.policy import Policy
 from crowd_sim.envs.utils.action import ActionXY
 
@@ -7,13 +7,10 @@ from crowd_sim.envs.utils.action import ActionXY
 class SocialForce(Policy):
     def __init__(self):
         super().__init__()
-        self.name = 'SocialForce'
+        self.name = "SocialForce"
         self.trainable = False
         self.multiagent_training = None
-        self.kinematics = 'holonomic'
-        self.initial_speed = 1
-        self.v0 = 10
-        self.sigma = 0.3
+        self.kinematics = "holonomic"
         self.sim = None
 
     def configure(self, config):
@@ -29,8 +26,17 @@ class SocialForce(Policy):
         :return:
         """
         sf_state = []
-        self_state = state.self_state
-        sf_state.append((self_state.px, self_state.py, self_state.vx, self_state.vy, self_state.gx, self_state.gy))
+        robot_state = state.robot_state
+        sf_state.append(
+            (
+                robot_state.px,
+                robot_state.py,
+                robot_state.vx,
+                robot_state.vy,
+                robot_state.gx,
+                robot_state.gy,
+            )
+        )
         for human_state in state.human_states:
             # approximate desired direction with current velocity
             if human_state.vx == 0 and human_state.vy == 0:
@@ -39,35 +45,14 @@ class SocialForce(Policy):
             else:
                 gx = human_state.px + human_state.vx
                 gy = human_state.py + human_state.vy
-            sf_state.append((human_state.px, human_state.py, human_state.vx, human_state.vy, gx, gy))
-        sim = socialforce.Simulator(np.array(sf_state), delta_t=self.time_step, initial_speed=self.initial_speed,
-                                    v0=self.v0, sigma=self.sigma)
+            sf_state.append(
+                (human_state.px, human_state.py, human_state.vx, human_state.vy, gx, gy)
+            )
+        groups = None  # add group info here
+        sim = Simulator(np.array(sf_state), groups=groups)
         sim.step()
         action = ActionXY(sim.state[0, 2], sim.state[0, 3])
 
         self.last_state = state
 
         return action
-
-
-class CentralizedSocialForce(SocialForce):
-    """
-    Centralized socialforce, a bit different from decentralized socialforce, where the goal position of other agents is
-    set to be (0, 0)
-    """
-    def __init__(self):
-        super().__init__()
-
-    def predict(self, state):
-        sf_state = []
-        for agent_state in state:
-            sf_state.append((agent_state.px, agent_state.py, agent_state.vx, agent_state.vy,
-                             agent_state.gx, agent_state.gy))
-
-        sim = socialforce.Simulator(np.array(sf_state), delta_t=self.time_step, initial_speed=self.initial_speed,
-                                    v0=self.v0, sigma=self.sigma)
-        sim.step()
-        actions = [ActionXY(sim.state[i, 2], sim.state[i, 3]) for i in range(len(state))]
-        del sim
-
-        return actions
