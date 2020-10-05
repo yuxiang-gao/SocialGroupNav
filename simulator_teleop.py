b4832634ff8027b13836da43218a738d301df1e0
Desktop-HS <hyshi97@gmail.com>
2020-10-05 15:36:05 -0400

    new

--- simulator_teleop.py
+++ simulator_teleop.py
@@ -78,26 +78,18 @@ while 1:
                 state = 1
                 speed[0] = 0
                 speed[1] = -1
-                #rect.top -=2
-                #bot.moveup()
             if event.key == K_COMMA:
                 state = 2
                 speed[0] = 0
                 speed[1] = 1
-                #rect.top += 2
-                #bot.movedown
             if event.key == K_j:
                 state = 3
                 speed[0] = -1
                 speed[1] = 0
-                #rect.left -= 2
-                #bot.moveleft()
             if event.key == K_l:
                 state = 4
                 speed[0] = 1
                 speed[1] = 0
-                #rect.left += 2
-                #bot.moveright()
             if event.key == K_u:
                 state = 5
                 speed[0] = -1
@@ -117,38 +109,34 @@ while 1:
             if event.key == K_k:
                 state = 0
                 speed[0] = 0
-                speed[1] = 0
-                #player.movepos = [0, 0]
-                #player.stat = still    
+                speed[1] = 0   
     human_colors = [cmap(i) for i in range(len(self.humans))]
-    for k in range(len(self.states)):
-        if k % 4 == 0 or k == len(self.states) - 1:
-            center_rect = robot_positions[k]
-            speed = (self.states[k - 1][0].px, self.states[k][0].py)
-            pygame.draw.line(screen, BLACK, center_rect, add(center_rect, (20* np.array(speed))), 3)
-            rect = bot.get_rect(center = robot_positions[k])
-            bot.fill(BLUE)
-            rect.top += speed[1]
-            rect.left += speed[0]
-            if rect.top < 0:
-                rect.top = 0
-            elif rect.bottom > 600:
-                rect.bottom = 600
-            elif rect.left < 0:
-                rect.left = 0
-            elif rect.right > 600:
-                rect.right = 600
+    #for k in range(len(self.states)):
+    k = pygame.time.get_ticks()
+    if k % 4 == 0 or k == len(self.states) - 1:
+        rect = robot_positions[-1]
+        pygame.draw.line(screen, BLACK, center_rect, add(center_rect, (20* np.array(speed))), 3)
+        rect = bot.get_rect(center = robot_positions[k])
+        bot.fill(BLUE)
+        rect.top += speed[1]
+        rect.left += speed[0]
+        if rect.top < 0:
+            rect.top = 0
+        elif rect.bottom > 600:
+            rect.bottom = 600
+        elif rect.left < 0:
+            rect.left = 0
+        elif rect.right > 600:
+            rect.right = 600
 
-            for i in range(len(self.humans)):
-                color = human_colors[i]
-                human = self.humans[i]
-                des = human.get_goal_position()
-                human_direction = (self.states[k - 1][1][i].px, self.states[k][1][i].py)
-                pygame.draw.circle(screen, color, des, 10)
-                pygame.draw.line(screen, BLACK, human_positions[j][i], add(d, (10* np.array(human_direction))), 3)
-                if human_positions[j][i] != des:
-                    human_positions[j][i] = add(human_positions[j][i], human_direction)     
-            
+        for i in range(len(self.humans)):
+                
+            color = human_colors[i]
+            human = self.humans[i]
+            human_direction = (self.states[k - 1][1][i].px, self.states[k][1][i].py)
+                
+            pygame.draw.circle(screen, color, human_positions[j][i], 10)
+            pygame.draw.line(screen, BLACK, human_positions[j][i], add(d, (10* np.array(human_direction))), 3)   
     
     screen.blit(bot, rect)
     pygame.display.update()
