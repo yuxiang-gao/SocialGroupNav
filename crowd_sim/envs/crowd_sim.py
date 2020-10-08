257c536a8ef3a9c73c7eb1c424901f5958cc9ce8
Desktop-HS <hyshi97@gmail.com>
2020-10-08 01:25:11 -0400

    crowd_sim change

--- crowd_sim/envs/crowd_sim.py
+++ crowd_sim/envs/crowd_sim.py
@@ -15,10 +15,9 @@ from crowd_sim.envs.utils.human import Human
 from crowd_sim.envs.utils.info import *
 from crowd_sim.envs.utils.utils import point_to_segment_dist
 from crowd_sim.envs.utils.scenarios import Scenario, ScenarioConfig, SceneManager
+from crowd_sim.envs.utils.render_teleop import render_teleop
 
-import sys
-import pygame
-from pygame.locals import *
+
 
 
 class CrowdSim(gym.Env):
@@ -415,115 +414,19 @@ class CrowdSim(gym.Env):
         display_numbers = True
 
         if mode == "teleop":
-            pygame.init()
-            size = width, height = 600, 600
-            speed = [2, 2]
-            WHITE = [255, 255, 255]
-            BLACK = [0, 0, 0]
-            RED = [255, 0, 0]
-            GREEN = [0, 255, 0]
-            BLUE = [0, 0, 255]
-            screen = pygame.display.set_mode(size)
-
-            bot = pygame.Surface((20, 20))
             robot_positions = [self.states[i][0].position for i in range(len(self.states))]
             human_positions = [
                 [self.states[i][1][j].position for j in range(len(self.humans))]
                 for i in range(len(self.states))
             ]
-            rect = bot.get_rect(center=(robot_positions[0][0]))
-            bot.fill(BLUE)
-            state = 0
-            screen.fill(WHITE)
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-                elif event.type == KEYDOWN:
-                    if event.key == K_i:
-                        state = 1
-                        speed[0] = 0
-                        speed[1] = -1
-                    if event.key == K_COMMA:
-                        state = 2
-                        speed[0] = 0
-                        speed[1] = 1
-                    if event.key == K_j:
-                        state = 3
-                        speed[0] = -1
-                        speed[1] = 0
-                    if event.key == K_l:
-                        state = 4
-                        speed[0] = 1
-                        speed[1] = 0
-                    if event.key == K_u:
-                        state = 5
-                        speed[0] = -1
-                        speed[1] = -1
-                    if event.key == K_o:
-                        state = 6
-                        speed[0] = 1
-                        speed[1] = -1
-                    if event.key == K_m:
-                        state = 7
-                        speed[0] = -1
-                        speed[1] = 1
-                    if event.key == K_PERIOD:
-                        state = 8
-                        speed[0] = 1
-                        speed[1] = 1
-                    if event.key == K_k:
-                        state = 0
-                        speed[0] = 0
-                        speed[1] = 0
-            human_colors = [cmap(i) for i in range(len(self.humans))]
-            prev_x = robot_positions[-1]
-            prev_y = robot_positions[-1]
+      
             for k in range(len(self.states)):
                 global_time = k * self.time_step
                 if k % 4 == 0 or k == len(self.states) - 1:
-                    rect = robot_positions[-1]
-                    pygame.draw.line(
-                        screen,
-                        BLACK,
-                        center_rect,
-                        (np.array(center_rect) + (20 * np.array(speed))),
-                        3,
-                    )
-                    rect = bot.get_rect(center=robot_positions[-1])
-                    bot.fill(BLUE)
-                    rect.top += speed[1]
-                    rect.left += speed[0]
-                    if rect.top < 0:
-                        rect.top = 0
-                    elif rect.bottom > 600:
-                        rect.bottom = 600
-                    elif rect.left < 0:
-                        rect.left = 0
-                    elif rect.right > 600:
-                        rect.right = 600
-
-                    for i in range(len(self.humans)):
-
-                        color = human_colors[i]
-                        human = self.humans[i]
-                        human_direction = (self.states[k - 1][1][i].px, self.states[k][1][i].py)
-
-                        pygame.draw.circle(screen, color, human_positions[j][i], 10)
-                        pygame.draw.line(
-                            screen,
-                            BLACK,
-                            human_positions[j][i],
-                            (np.array(d) + (10 * np.array(human_direction))),
-                            3,
-                        )
-
-            screen.blit(bot, rect)
-            pygame.display.update()
-            theta = atan2(speed[1], speed[0])
-            robot.theta = theta
-            vx = np.cos(theta) * x + np.sin(theta) * y
-            vy = np.cos(theta) * y + np.sin(theta) * x
-            return (vx, vy)
+                    center = robot_positions[-1]
+                    robot_render = pyg_render(center[0], center[1], 600, 600)
+                    robot_render.run()
+              
 
         elif mode == "traj":
             fig, ax = plt.subplots(figsize=(7, 7))
